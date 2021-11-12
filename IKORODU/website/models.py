d2175d39c9c95ef2c7fe0261c1d68dc4cba9d8d7
Redx <78570858+Unique-Red@users.noreply.github.com>
2021-11-12 13:34:35 +0100

    Add files via upload

--- IKORODU/website/models.py
+++ IKORODU/website/models.py
@@ -1,5 +1,6 @@
 from website import db
 from sqlalchemy.sql import func
+from flask_login import UserMixin
 
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -8,3 +9,8 @@ class Post(db.Model):
     file = db.Column(db.String(150), nullable=False)
     date_created = db.Column(db.DateTime(timezone=True), default=func.now())
 
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), nullable=False, unique=True)
+    password = db.Column(db.String(30), nullable=False)
+    date_created = db.Column(db.DateTime(timezone=True), default=func.now())
--- IKORODU/website/routes.py
+++ IKORODU/website/routes.py
@@ -1,6 +1,8 @@
 from flask import render_template, session, request, redirect, url_for, flash
 from sqlalchemy.sql.functions import current_user
-from .models import Post
+from .models import Post, User
+from flask_login import login_user, logout_user, login_required
+from werkzeug.security import generate_password_hash, check_password_hash
 from website import app, db
 import os
 
@@ -29,7 +31,7 @@ def project():
     posts = Post.query.order_by(Post.date_created.desc()).all()
     return render_template ("project.html", posts=posts)
 
-@app.route("/auth", methods=["GET", "POST"])
+'''@app.route("/auth", methods=["GET", "POST"])
 def auth():
     if request.method == "POST":
         if request.form.get("username") == "Red" and request.form.get("password") == "red@123":
@@ -37,14 +39,68 @@ def auth():
             return redirect("/create")
         else:
             return render_template("admin/auth.html", failed=True)
+    return render_template ("admin/auth.html")'''
+
+@app.route("/auth", methods=["GET", "POST"])
+def auth():
+    if request.method == "POST":
+        username = request.form.get("username")
+        password = request.form.get("password")
+
+        user = User.query.filter_by(username=username).first()
+        if user:
+            if check_password_hash(user.password, password):
+                login_user(user, remember=True)
+                return redirect(url_for('table'))
+            else:
+                flash("Password is incorrect", category='error') 
+        else:
+            flash("Username does not exist", category='error')
+
     return render_template ("admin/auth.html")
 
+
+@app.route("/reg", methods=["GET", "POST"])
+def reg():
+    if request.method == "POST":
+        username = request.form.get("username")
+        password1 = request.form.get("password1")
+        password2 = request.form.get("password2")
+
+        username_exists = User.query.filter_by(username=username).first()
+        if username_exists:
+            flash("Username in use.", category='error') 
+        elif password1 != password2:
+            flash("Password don't match!", category='error') 
+        elif len(username) < 2:
+            flash("Username is too short", category='error') 
+        elif len(password1) < 6:
+            flash("Password is too short", category='error')
+        else:
+            new_user = User(username=username, password=generate_password_hash(password1, method='sha256'))
+            db.session.add(new_user)
+            db.session.commit()
+
+            return redirect(url_for('table'))
+
+
+    return render_template ("admin/register.html")
+    
+
 @app.route("/logout")
+@login_required
 def logout():
-    session.clear()
+    logout_user()
     return redirect("/")
 
+
+'''@app.route("/logout")
+def logout():
+    session.clear()
+    return redirect("/")'''
+
 @app.route("/create", methods=['GET', 'POST'])
+@login_required
 def create():
     if request.method == "POST":
             title = request.form.get("title")
@@ -65,51 +121,107 @@ def create():
     return render_template ("admin/create.html")
 
 @app.route("/table", methods=["GET","POST"])
+@login_required
 def table():
     posts = Post.query.all()
     return render_template ("admin/table.html", posts=posts)
 
 @app.route("/title", methods=["GET","POST"])
+@login_required
 def title():
     posts = Post.query.all()
     return render_template ("admin/title.html", posts=posts)
 
 @app.route("/text", methods=["GET","POST"])
+@login_required
 def text():
     posts = Post.query.all()
     return render_template ("admin/text.html", posts=posts)
 
+@app.route("/file", methods=["GET","POST"])
+@login_required
+def file():
+    posts = Post.query.all()
+    return render_template ("admin/files.html", posts=posts)
+
+
+
 @app.route("/updatetitle/<int:id>", methods=['GET','POST'])
+@login_required
 def updatetitle(id):
-    updatetitle = Post.query.get(id)
-    title = request.form.get("title")
+    title_to_update = Post.query.get_or_404(id)
     if request.method=="POST":
-        updatetitle.title = title
-        flash(f"Your project has been updated","success")
-        db.session.commit()
-        return redirect(url_for("title"))
-    return render_template("admin/updatetitle.html", updatetitle=updatetitle)
+        title_to_update.title = request.form['title']
+        try:
+            db.session.commit()
+            return redirect('/title')
+
+        except:
+            flash("There was a problem updating this title")
+    else:
+        return render_template("admin/updatetitle.html", title_to_update=title_to_update)
 
 
 
 @app.route("/updatetext/<int:id>", methods=["GET","POST"])
+@login_required
 def updatetext(id):
-    updatetext = Post.query.get(id)
-    text = request.form.get("text")
+    text_to_update = Post.query.get_or_404(id)
     if request.method=="POST":
-        updatetext.text = text
-        flash(f"Your project has been updated","success")
-        db.session.commit()
-        return redirect(url_for("text"))
+        text_to_update.text = request.form['text']
+        try:
+            db.session.commit()
+            return redirect('/text')
 
-    return render_template("admin/updatetext.html", updatetext=updatetext)
+        except:
+            flash("There was a problem updating this project")
+    else:
+        return render_template("admin/updatetext.html", text_to_update=text_to_update)
+
 
 @app.route("/updatetable/<int:id>", methods=["GET","POST"])
+@login_required
 def updatetable(id):
     posts = Post.query.get_or_404(id)
-    title = request.form.get("title")
-    text = request.form.get("text")
-    text = request.form.get("text")
-    date_created = request.form.get("date_created")
-    return render_template("admin/updatetable.html", posts=posts, title=title)
+    if request.method == 'POST':
+        title = request.form.get("title")
+        text = request.form.get("text")
+        file = request.files["file"]
+        posts.title = title
+        posts.text = text
+        file.save(os.path.join(os.path.abspath(__package__),"static/uploads/"+file.filename))
+        posts.file = file.filename
+        flash("Edited Successfully!", "Success")
+        db.session.commit()
+    return render_template("admin/updatetable.html", posts=posts)
 
+@app.route("/updatefile/<int:id>", methods=['GET','POST'])
+@login_required
+def updatefile(id):
+    posts = Post.query.get_or_404(id)
+    if request.method=="POST":
+        posts.file = request.files['file']
+        file.save(os.path.join(os.path.abspath(__package__),"static/uploads/"+file.filename))
+        posts.file = file.filename
+        try:
+            db.session.commit()
+            return redirect('/file')
+
+        except:
+            flash("There was a problem updating this file")
+    else:
+        return render_template("admin/updatefile.html", posts=posts)
+
+
+@app.route("/delete/<int:id>")
+@login_required
+def delete(id):
+    project_delete = Post.query.get_or_404(id)
+
+    try:
+        db.session.delete(project_delete)
+        db.session.commit()
+        return redirect("/table")
+
+    except:
+        flash("There was a problem deleting this project", category='error')
